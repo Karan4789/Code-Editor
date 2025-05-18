7de5ac1a5641410ad6ca38a91e276d4f6033665e
Karan4789 <karan2301.m@gmail.com>
2025-05-18 09:54:25 +0530

    done

--- Server/server.js
+++ Server/server.js
@@ -9,12 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS for production
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? [
-        'https://karan4789.github.io',
-        'http://localhost:5173'
-      ]
-    : '*'
+  origin: ['https://karan4789.github.io', 'http://localhost:5173'],
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
 
 app.use(express.json());
